b7d922210ffc00c053605308006d7790b4ad3b21
Robin <cuentadersab2@gmail.com>
2023-07-18 23:59:04 -0500

    functionality on the page

--- scripts/education-scritpt.js
+++ scripts/education-scritpt.js
@@ -1,3 +1,16 @@
+const storedText = localStorage.getItem('textIn');
+document.querySelector('.js-note-paragraph').innerHTML = storedText;
+
 function saveTheText() {
-  console.log('text saved');
+  const textInput = document.querySelector('.js-note-input').value;
+  
+  document.querySelector('.js-note-paragraph').innerHTML = textInput;
+
+  localStorage.setItem('textIn', textInput);
 }
+
+function removeNote() {
+  console.log('Do nothing yet');
+}
+
+// setItem here. I have to set textInput so when I open the web the variable won't get reset.
