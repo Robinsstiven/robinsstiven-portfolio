1d99a1a54bde53c32bddab1ea40070265486d387
Robin <cuentadersab2@gmail.com>
2023-07-31 09:39:34 -0500

    code cleaned

--- scripts/index-functionality.js
+++ scripts/index-functionality.js
@@ -1,1 +1,4 @@
-console.log('no functionality yet');
+/* function updateTheStyle () {
+  const aboutButtonElem = document.querySelector('.js-about-button');
+  aboutButtonElem.classList.add('about-me-button');
+} */
