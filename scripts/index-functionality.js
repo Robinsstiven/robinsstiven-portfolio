f4e65e76a879bc3dac7e1ec5fc8617ba70357226
Robin <cuentadersab2@gmail.com>
2023-08-09 11:12:03 -0500

    menu

--- scripts/index-functionality.js
+++ scripts/index-functionality.js
@@ -1,9 +1,9 @@
 function displayMenu() {
   const responsiveMenu = document.querySelector('.js-nav-menu');
 
-  if (responsiveMenu.classList.contains('responsive-menu')) {
-    responsiveMenu.classList.add('responsive-menu-show');
-  } else if (responsiveMenu.classList.contains('responsive-menu-show') && responsiveMenu.classList.contains('responsive-menu')) {
+  if (responsiveMenu.classList.contains('responsive-menu-show')) {
     responsiveMenu.classList.remove('responsive-menu-show');
+  } else if (responsiveMenu.classList.contains('responsive-menu')) {
+    responsiveMenu.classList.add('responsive-menu-show');
   }
 }
