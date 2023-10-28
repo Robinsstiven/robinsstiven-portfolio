3a2df7b999b76648f15e7f393de3f5ad9261d30a
Robin in Mac <cuentadersab2@gmail.com>
2023-10-28 14:58:12 -0500

    Back to top button created. Scroll effect added.

--- scripts/index-functionality.js
+++ scripts/index-functionality.js
@@ -7,3 +7,31 @@ function displayMenu() {
     responsiveMenu.classList.add('responsive-menu-show');
   }
 }
+
+const aboutSectionButton = document.querySelector('.js-about-button');
+const aboutSection = document.querySelector('.js-about-section');
+
+aboutSectionButton.addEventListener('click', () => {
+  aboutSection.scrollIntoView({behavior: 'smooth'});
+});
+
+const text = 'Robins portfolio';
+console.log(btoa(text));
+
+const goToTopButton = document.querySelector('.js-goto-the-top');
+
+
+goToTopButton.addEventListener('click', () => {
+  window.scrollTo ({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 140) {
+    goToTopButton.style.display = 'block';
+  } else {
+    goToTopButton.style.display = 'none';
+  }
+});
